478f7493bcfad43b2e437eb7736b9098d32d4d29
Aerobautics <48452349+Aerobautics@users.noreply.github.com>
2019-09-22 10:09:55 -0700

    Added initial code
    Added initial code to repository. Needs changes.

---
+++ april_tag_generator_python.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+def mainFunction():
+	displayMenu()
+	
+def displayMenu():
+	print("0. Exit")
+	print("1. Generate sequential images")
+	print("2. Change start value")
+	print("3. Change end value")
+	print("4. Change filename")
+	print("5. Generate normal images")
+	print("6. Change mean")
+	print("7. Change deviation")
+	print("8. Change number of images")
+	print("9. Rotate")
+	print("10. Apply background")
+	print("11. Help")
+	print("Enter choice: ")
+	
+if __name__ == "__main__":
+	mainFunction()	
+	
