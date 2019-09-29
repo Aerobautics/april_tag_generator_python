b3bce39c8312ec7b4bc838f635a0a2cdedbf5ebb
Aerobautics <48452349+Aerobautics@users.noreply.github.com>
2019-09-28 19:08:00 -0700

    Added background image
    Added background image. Still needs code for normal distribution. Code is rough draft and not final. Not everything works correctly.

--- april_tag_generator_python.py
+++ april_tag_generator_python.py
@@ -6,11 +6,22 @@ def mainFunction():
 	maximumValue = 68719476735
 	mean = maximumValue // 2
 	nominalValue = 2863311530
-	choice = 12
-	startValue = 0
-	endValue = 0
-	backgroundImage
-	angles
+	deviation = 1000
+	numberOfImages = 1
+	choice = 255
+	startValue = 2863311530
+	endValue = startValue + 1
+	tagLength = 240
+	imageSize = 640
+	backgroundImage = []
+	angles = 0
+	rawAngles = []
+	filePath = 'G:\\Ubuntu\\apriltags\\'
+	fileName = 'output'
+	#backgroundFilename = 'G:\\Ubuntu\\apriltag_generator_python\\apriltag_generator_python\\background.jpg'
+	backgroundFilename = 'background.jpg'
+	isBackgroundLoaded = False
+	isBackgroundApplied = False
 	while choice:
 		displayMenu()
 		choice_string = input("Enter choice: ")
@@ -18,10 +29,17 @@ def mainFunction():
 		if choice == 0:	
 			print(choice)
 		elif choice == 1:
-			for i in range(startValue, endValue):
-				temporary = generateArray(i)
-				temporary = generateImageValues(temporary)
-				generateImage(temporary)
+			if angles == 0:
+				for i in range(startValue, endValue):
+					temporary = generateArray(i)
+					temporary = generateImageValues(temporary)
+					generateImage(temporary, tagLength, imageSize, angles, backgroundImage, isBackgroundApplied)			
+			else:
+				for j in range(len(angles)):
+					for i in range(startValue, endValue):
+						temporary = generateArray(i)
+						temporary = generateImageValues(temporary)
+						generateImage(temporary, tagLength, imageSize, angles[j], backgroundImage, isBackgroundApplied)
 		elif choice == 2:
 			startValue = input('Enter start value: ')
 			startValue = int(startValue)
@@ -29,27 +47,67 @@ def mainFunction():
 			endValue = input('Enter end value: ')
 			endValue = int(endValue)
 		elif choice == 4:
-			print(choice)
+			print('Current filename: ', fileName)
+			fileName = input('Enter new filename: ')
+			print('New filename: ', fileName)
 		elif choice == 5:
-			print(choice)
+			print('Current file path: ', filePath)
+			filePath = input('Enter new file path: ')
+			print('New file path: ', filePath)
 		elif choice == 6:
 			temporary = generateArray(nominalValue)
 			temporary = generateImageValues(temporary)
 			generateImage(temporary)
 		elif choice == 7:
-			print(choice)
+			print('Current mean: ', mean)
+			mean = input('Enter new mean: ')
+			mean = int(mean)
+			print('New mean: ', mean)			
 		elif choice == 8:
-			print(choice)
+			print('Current deviation: ', deviation)
+			deviation = input('Enter new deviation: ')
+			deviation = int(deviation)
+			print('New deviation: ', deviation)
 		elif choice == 9:
-			print(choice)
+			print('Current current number of images: ', numberOfImages)
+			numberOfImages = input('Enter new number of images: ')
+			numberOfImages = int(numberOfImages)
+			print('New number of images: ', numberOfImages)
 		elif choice == 10:
-			print(choice)
+			print('Current angles: ', angles)
+			rawAngles = input('Enter angles: ')
+			rawAngles = rawAngles.split(',')
+			rawAngles = [int(i) for i in rawAngles]
+			angles = rawAngles
+			print('New angles: ', angles)
 		elif choice == 11:
+			if isBackgroundApplied:
+				isBackgroundApplied = False
+				print('Background image off.')
+			else:
+				if isBackgroundLoaded:
+					isBackgroundApplied = True
+				else:
+					#backgroundImage = cv2.imread(backgroundFilename, cv2.IMREAD_COLOR)
+					backgroundImage = cv2.imread(backgroundFilename, cv2.IMREAD_GRAYSCALE)
+					isBackgroundLoaded = True
+					isBackgroundApplied = True
+				print('Background image on.')
+		elif choice == 12:
+			print(choice)
+		elif choice == 13:
+			print('Current background: ', backgroundFilename)
+			backgroundFilename = input('Enter new background image: ')
+			print('New background file: ', backgroundFilename)
+		elif choice == 14:
+			print(choice)
+		elif choice == 15:
+			printHelp()
+		elif choice == 16:
 			print(choice)
 		else:
 			print(choice)
-	return
-	
+	return	
 	
 def displayMenu():
 	print("----Menu----")
@@ -58,25 +116,29 @@ def displayMenu():
 	print("2. Change start value")
 	print("3. Change end value")
 	print("4. Change filename")
-	print("5. Generate normal images")
-	print("6. Change mean")
-	print("7. Change deviation")
-	print("8. Change number of images")
-	print("9. Rotate")
-	print("10. Apply background")
-	print("11. Help")
+	print("5. Change file path")
+	print("6. Generate normal images")
+	print("7. Change mean")
+	print("8. Change deviation")
+	print("9. Change number of images")
+	print("10. Rotation angles")
+	print("11. Apply/remove background")
+	print("12. Assign background image")
+	print("13. Adjust tag length")
+	print("14. Adjust image size")
+	print("15. Help")
+	print("16. Settings")
 	return
 	
-def generateImage(input_array):
+def generateImage(input_array, tag_width, image_width, image_angle, background_image, apply_background = False):
 	print("generateImage()")
-	imageWidth = 480
 	maximumPixelStrength = 255
 	halfMaximumPixelStrength = 124	
 	output = np.array(input_array, dtype = np.float32)
 	a = output.shape[0]
 	b = output.shape[1]
 	output = np.multiply(output, maximumPixelStrength)
-	while a < imageWidth:
+	while a < tag_width:
 		output = output.astype(np.float32)
 		output = np.multiply(output, 1.0 / maximumPixelStrength)
 		a *= 2
@@ -86,6 +148,20 @@ def generateImage(input_array):
 		output = np.rint(output)
 		output = output.astype(np.uint8)
 		retval, output = cv2.threshold(output, halfMaximumPixelStrength, maximumPixelStrength, cv2.THRESH_BINARY)
+	borderSize = image_width - tag_width
+	borderSize = borderSize // 2
+	if apply_background:
+		background_image = cv2.resize(background_image, (image_width, image_width))
+		borderSize = image_width - tag_width
+		borderSize = borderSize // 2
+		background_image[borderSize : borderSize + output.shape[0], borderSize : borderSize + output.shape[1]] = output
+		output = background_image
+	else:	
+		if image_width - tag_width > 0:
+			output = cv2.copyMakeBorder(output, top = borderSize, bottom = borderSize, left = borderSize, right = borderSize, borderType = cv2.BORDER_CONSTANT, value = [0, 0, 0])
+	centerLocation = (image_width / 2, image_width / 2)	
+	rotationMatrix = cv2.getRotationMatrix2D(centerLocation, image_angle, 1.0)
+	output = cv2.warpAffine(output, rotationMatrix, (image_width, image_width))
 	cv2.imshow('Apriltag', output)
 	cv2.waitKey(300)
 	return
@@ -151,6 +227,24 @@ def generateImageValues(boolean_array):
 		print()
 	return output
 
+def printHelp():
+	print("(0) Exit - Exit application")
+	print("(1) Generate sequential images - Create images from start value to end value")
+	print("(2) Change start value - Change start value")
+	print("(3) Change end value - Change end value")
+	print("(4) Change filename - Change filename under which images are stored")
+	print("(5) Change file path - Change the path under which images are stored")
+	print("(6) Generate normal images - Create images with a random value of normal distribution")
+	print("(7) Change mean - Change mean which is is used for generating normal image distribution")
+	print("(8) Change deviation - Change standard deviation of normal image distribution")
+	print("(9) Change number of images - Change the number of images generated with (6)")
+	print("(10) Rotation angles - Enter the angles at which the Apriltags will be rotated")
+	print("(11) Apply/remove background - Apply or remove the current background image")
+	print("(12) Assign background image -  Change filename of background image")
+	print("(13) Adjust tag lengt - Change the length of the Apriltag image")
+	print("(14) Adjust image size - Change the size of the image which contains the Apriltag")
+	print("(15) Help - Display information on selections")
+	
 if __name__ == "__main__":
 	mainFunction()	
 	
